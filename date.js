1000c8da78ed83c546e4a45387eb415ddfbd275b
Laurent Maxhuni <152623037+LaurentMaxhuni@users.noreply.github.com>
2024-04-03 18:25:36 +0200

    Add files via upload

--- date.js
+++ date.js
@@ -1,9 +1,22 @@
 let date = new Date();
-date.setDate(date.getDate() + 1); // increment the date by 1
-let daysOfWeek = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
-
-for (let i = 1; i < 9; i++) {
-    let dayName = daysOfWeek[date.getDay()];
-    document.getElementById('day' + i).innerHTML = dayName;
-    date.setDate(date.getDate() + 1); // increment the date by 1
+let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let monthOfYear = ['January','February','March','April','May','June','July','August','September','October','November','December']
+let monthIndex = date.getMonth();
+let month = monthOfYear[monthIndex];
+let datee = date.getDate();
+if(datee == 1) { 
+    datee = datee + 'st';
+} else if(datee == 2) {
+    datee = datee  + 'nd';
+} else if(datee == 3) {
+    datee = datee + 'rd';
+} else if(datee > 3 && datee < 31) {
+    datee = datee + 'th';
+} else if(datee == 31) {
+    datee = datee  + 'st';
 }
+let dayIndex = date.getDay();
+let day = daysOfWeek[dayIndex];
+let year = date.getFullYear();
+document.getElementById('date').innerHTML = `<p><i class="bi bi-calendar3 mx-1"></i>${month} ${datee} ${year} / ${day}</p>`;
+
